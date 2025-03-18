eb42069d5f7f68bfa71511920ee6505db5905c3f
rafacezimbra <rafael.rodrigues@aln.senaicimatec.edu.br>
2025-03-18 08:46:11 -0300

    atualizacao lista2_ex7

--- lista2_ex7/src/entidades/Quarto.java
+++ lista2_ex7/src/entidades/Quarto.java
@@ -1,5 +1,16 @@
 package entidades;
 
+
 public class Quarto {
+	
+	private Estudante inquilino;
+
+	public Estudante getInquilino() {
+		return inquilino;
+	}
+
+	public void setInquilino(Estudante inquilino) {
+		this.inquilino = inquilino;
+	}
 
 }
